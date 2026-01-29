78437656e996facfefeb29df45510be2e1c47a24
suryasekhar2003 <suryasekhardhal2003@gmail.com>
2026-01-29 23:42:49 +0530

    feat: update createCategory to accept description and image from request body

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -4,7 +4,8 @@ import { ApiResponce } from "../utils/ApiResponce.js";
 import { Category } from "../models/category.model.js";
 
 const createCategory = asyncHandler(async (req, res) => {
-    const {name} = req.body
+    const {name,description,image} = req.body
+
 });
 
 export { createCategory };
