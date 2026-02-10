dfbf39a92e898a6623e560c050ceef8ae9a0561b
suryasekhar2003 <suryasekhardhal2003@gmail.com>
2026-02-10 14:53:19 +0530

    feat: add rating controller with necessary imports for handling ratings

---
+++ src/controllers/rating.controller.js
@@ -0,0 +1,5 @@
+import {Rating} from "../models/rating.model.js";
+import {ApiError} from "../utils/ApiError.js";
+import {Product} from "../models/product.model.js";
+import { ApiResponce } from "../utils/ApiResponce.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
